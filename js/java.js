cff397b08e37d332c64d030f84324389ed3c3a5a
Clayton <56970620+srclayton@users.noreply.github.com>
2020-12-02 17:35:12 -0300

    Adicionei as pastas 'Code string' e 'Credit card num...' na versão desktop

--- js/java.js
+++ js/java.js
@@ -56,9 +56,46 @@ function abrirPastaDois(){
     document.getElementById("button-home").innerHTML="<button style='margin-top: 5px; margin-left: 5px;' onclick='PastaProjetos()'> <img src='img/home.png' width='100%'></button>"
 	  document.getElementById("pastaPrincipal").innerHTML="<div class='container'>"+
                                                             "<div class='row'>"+
-                                                                "<div class='col-2'>"+
-                                                                    "<br> <a href='https://github.com/srclayton/C-Programming/tree/master/credit%20card%20number%20validator' target='_blank'><button><img src='img/github.png' width='80px'height='80px'><p>card.c</p></button></a>" +
+                                                                        "<div class='col-4'>"+
+                                                                        " Nome"+
+                                                                    "</div>"+
+                                                                    "<div class='col-4'>"+
+                                                                        "| Data de Modificação"+
+                                                                    "</div>"+
+                                                                    "<div class='col-4'>"+
+                                                                        "| Tipo"+
+                                                                    "</div>"+
                                                                 "</div>"+
+                                                                //pasta code//
+                                                                "<button onclick='abrirPastaCode()'style='width:98%; margin-top: 5px; margin-left: 5px '>"+
+                                                                "<div class='row' style='text-align:left'>"+
+                                                                    "<div class='col-4'>"+
+                                                                        "<img src='img/mini-pasta.png' width='25px'> Code string"+
+                                                                    "</div>"+
+                                                                    "<div class='col-4'>"+
+                                                                        " 02/12/2020 10:38pm"+
+                                                                    "</div>"+
+                                                                    "<div class='col-4'>"+
+                                                                        "  Pasta de Arquivos"+
+                                                                    "</div>"+
+                                                                "</div>"+
+                                                                "</button>"+
+                                                                //pasta code//
+                                                                //pasta decode//
+                                                                "<button onclick='abrirPastaCredtCard()' style='width:98%; margin-top: 5px; margin-left: 5px '>"+
+                                                                "<div class='row' style='text-align:left'>"+
+                                                                    "<div class='col-4'>"+
+                                                                        "<img src='img/mini-pasta.png' width='25px'> Credit card num..."+
+                                                                    "</div>"+
+                                                                    "<div class='col-4'>"+
+                                                                        " 26/04/2020 18:48pm"+
+                                                                    "</div>"+
+                                                                    "<div class='col-4'>"+
+                                                                        "  Pasta de Arquivos"+
+                                                                    "</div>"+
+                                                                "</div>"+
+                                                                "</button>"+
+                                                                //pasta decode//
                                                             "</div>"+
                                                         "</div>";
     document.getElementById("janela-pesquisa").innerHTML="<img src='img/mini-pasta.png' width='22px'> &gt; <button data-dismiss='modal' aria-label='Close'> Desktop </button> &gt; <button onclick='PastaProjetos()'>Projetos</button> &gt; <button onclick='abrirPastaDois()'>Programação em C</button>";
@@ -88,6 +125,113 @@ function abrirPastaSeis(){
     document.getElementById("janela-pesquisa").innerHTML="<img src='img/mini-pasta.png' width='22px'> &gt; <button data-dismiss='modal' aria-label='Close'> Desktop </button> &gt; <button onclick='PastaProjetos()'>Projetos</button> &gt; <button onclick='abrirPastaSeis()'>Pasta Seis</button>";
 }
 // CONTEUDOS DA PASTA "PROJETOS".
+
+//PASTA CODE STRING
+function abrirPastaCode(){
+    document.getElementById("pastaPrincipal").innerHTML="<div class='container'>"+
+                                                            "<div class='row'>"+
+                                                                        "<div class='col-4'>"+
+                                                                        " Nome"+
+                                                                    "</div>"+
+                                                                    "<div class='col-4'>"+
+                                                                        "| Data de Modificação"+
+                                                                    "</div>"+
+                                                                    "<div class='col-4'>"+
+                                                                        "| Tipo"+
+                                                                    "</div>"+
+                                                                "</div>"+
+                                                                //code string .c //
+                                                                "<button onclick='erroReprodizor()' data-toggle='modal' data-target='.bd-example-modal-sm' style='width:98%; margin-top: 5px; margin-left: 5px '>"+
+                                                                "<div class='row' style='text-align:left'>"+
+                                                                    "<div class='col-4'>"+
+                                                                        "<img src='img/unknown.png' width='25px'> code_string.c"+
+                                                                    "</div>"+
+                                                                    "<div class='col-4'>"+
+                                                                        " 02/12/2020 10:38pm"+
+                                                                    "</div>"+
+                                                                    "<div class='col-4'>"+
+                                                                        "  Arquivo Fonte C"+
+                                                                    "</div>"+
+                                                                "</div>"+
+                                                                "</button>"+
+                                                                //code string .c//
+                                                                //code string .c  github//
+                                                                "<a href='https://github.com/srclayton/C-Programming/tree/master/code%20string' target='_blank'><button style='width:98%; margin-top: 5px; margin-left: 5px '>"+
+                                                                "<div class='row' style='text-align:left'>"+
+                                                                    "<div class='col-4'>"+
+                                                                        "<img src='img/github.png' width='25px'> code_string.c"+
+                                                                    "</div>"+
+                                                                    "<div class='col-4'>"+
+                                                                        " 02/12/2020 10:42pm"+
+                                                                    "</div>"+
+                                                                    "<div class='col-4'>"+
+                                                                        "  HTML Document"+
+                                                                    "</div>"+
+                                                                "</div>"+
+                                                                "</button></a>"+
+                                                                //code string .c  github //
+                                                            "</div>"+
+                                                        "</div>";
+    document.getElementById("barra-lateral-pasta").innerHTML="<br><button style='width: 100%; margin-left: 7px; text-align: left;' data-dismiss='modal' aria-label='Close'> <img src='img/mini-pasta.png' width='22px'>Desktop</button>"+
+                                                            "<br><button style='width: 100%; margin-left: 7px; text-align: left;' onclick='PastaProjetos()'>    <img src='img/mini-pasta.png' width='22px'>Projetos</button>"+
+                                                            "<br><button style='width: 100%; margin-left: 7px; text-align: left;' onclick='abrirPastaDois()'>    <img src='img/mini-pasta.png' width='22px'>Programação em...</button>"+
+                                                            "<br><button style='width: 100%; margin-left: 7px; text-align: left;' onclick='abrirPastaCode()'>    <img src='img/mini-pasta.png' width='22px'>Code string</button>";
+    document.getElementById("janela-pesquisa").innerHTML="<img src='img/mini-pasta.png' width='22px'> &gt; <button data-dismiss='modal' aria-label='Close'> Desktop </button> &gt; <button onclick='PastaProjetos()'>Projetos</button> &gt; <button onclick='abrirPastaDois()'>Programação em C</button> &gt; <button onclick='abrirPastaCode()'>Code string</button>";
+}
+//PASTA CODE STRING
+// PASTA CREDIT CARD
+function abrirPastaCredtCard(){
+    document.getElementById("pastaPrincipal").innerHTML="<div class='container'>"+
+                                                            "<div class='row'>"+
+                                                                        "<div class='col-4'>"+
+                                                                        " Nome"+
+                                                                    "</div>"+
+                                                                    "<div class='col-4'>"+
+                                                                        "| Data de Modificação"+
+                                                                    "</div>"+
+                                                                    "<div class='col-4'>"+
+                                                                        "| Tipo"+
+                                                                    "</div>"+
+                                                                "</div>"+
+                                                                //credit card .c //
+                                                                "<button onclick='erroReprodizor()' data-toggle='modal' data-target='.bd-example-modal-sm' style='width:98%; margin-top: 5px; margin-left: 5px '>"+
+                                                                "<div class='row' style='text-align:left'>"+
+                                                                    "<div class='col-4'>"+
+                                                                        "<img src='img/unknown.png' width='25px'> card.c"+
+                                                                    "</div>"+
+                                                                    "<div class='col-4'>"+
+                                                                        " 02/12/2020 10:38pm"+
+                                                                    "</div>"+
+                                                                    "<div class='col-4'>"+
+                                                                        "  Arquivo Fonte C"+
+                                                                    "</div>"+
+                                                                "</div>"+
+                                                                "</button>"+
+                                                                //credit card .c//
+                                                                //credit card .c  github//
+                                                                "<a href='https://github.com/srclayton/Validador-de-cartao-de-credito' target='_blank'><button style='width:98%; margin-top: 5px; margin-left: 5px '>"+
+                                                                "<div class='row' style='text-align:left'>"+
+                                                                    "<div class='col-4'>"+
+                                                                        "<img src='img/github.png' width='25px'> card.c"+
+                                                                    "</div>"+
+                                                                    "<div class='col-4'>"+
+                                                                        " 02/12/2020 10:42pm"+
+                                                                    "</div>"+
+                                                                    "<div class='col-4'>"+
+                                                                        "  HTML Document"+
+                                                                    "</div>"+
+                                                                "</div>"+
+                                                                "</button></a>"+
+                                                                //credit card .c  github //
+                                                            "</div>"+
+                                                        "</div>";
+    document.getElementById("barra-lateral-pasta").innerHTML="<br><button style='width: 100%; margin-left: 7px; text-align: left;' data-dismiss='modal' aria-label='Close'> <img src='img/mini-pasta.png' width='22px'>Desktop</button>"+
+                                                            "<br><button style='width: 100%; margin-left: 7px; text-align: left;' onclick='PastaProjetos()'>    <img src='img/mini-pasta.png' width='22px'>Projetos</button>"+
+                                                            "<br><button style='width: 100%; margin-left: 7px; text-align: left;' onclick='abrirPastaDois()'>    <img src='img/mini-pasta.png' width='22px'>Programação em...</button>"+
+                                                            "<br><button style='width: 100%; margin-left: 7px; text-align: left;' onclick='abrirPastaCredtCard()'>    <img src='img/mini-pasta.png' width='22px'>Credit card num...</button>";
+    document.getElementById("janela-pesquisa").innerHTML="<img src='img/mini-pasta.png' width='22px'> &gt; <button data-dismiss='modal' aria-label='Close'> Desktop </button> &gt; <button onclick='PastaProjetos()'>Projetos</button> &gt; <button onclick='abrirPastaDois()'>Programação em C</button> &gt; <button onclick='abrirPastaCredtCard()'>Credit card number validator</button>";
+}
+// PASTA CREDIT CARD
 
 // PASTA NO DESKTOP COM NOME "CV WEB" ESSA FUNÇÃO ABRE UM MODAL COM AS INFORMAÇÕES DA PASTA.
 function abrirCvWeb(){
